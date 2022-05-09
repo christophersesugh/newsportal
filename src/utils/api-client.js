dd9b2dca39889730ebe2d5f3cca0eb6be72ad9d2
Christopher Sesugh <christohybrid185@gmail.com>
2022-05-09 12:45:08 +0100

    Update api-client.js

--- src/utils/api-client.js
+++ src/utils/api-client.js
@@ -1,5 +1,4 @@
 import * as auth from "auth-provider";
-const apiURL = process.env.REACT_APP_API_URL;
 
 async function client(
   endpoint,
@@ -16,25 +15,22 @@ async function client(
     ...customConfig,
   };
 
-  return (
-    window
-      .fetch(`https://api.thenewsapi.com/v1/news/${endpoint}`, config)
-      // .fetch(`${apiURL}${endpoint}`, config)
-      .then(async (response) => {
-        if (response.status === 401) {
-          await auth.logout();
-          // refresh the page for them
-          window.location.assign(window.location);
-          return Promise.reject({ message: "Please re-authenticate." });
-        }
-        const data = await response.json();
-        if (response.ok) {
-          return data;
-        } else {
-          return Promise.reject(data);
-        }
-      })
-  );
+  return window
+    .fetch(`https://api.thenewsapi.com/v1/news/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        // refresh the page for them
+        window.location.assign(window.location);
+        return Promise.reject({ message: "Please re-authenticate." });
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
 }
 
 export { client };
